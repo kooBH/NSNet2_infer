007da8ddb249ed353226f6c37e2a43db95f2f2c9
KooBH <ninie4409@sogang.ac.kr>
2023-11-22 13:57:39 +0900

    fix

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NSNet2_infer
 NSNet C++ inference for baseline evaluation
+
+https://github.com/microsoft/DNS-Challenge/tree/v4dnschallenge_ICASSP2022/NSNet2-baseline  
--- RTF.py
+++ RTF.py
@@ -10,7 +10,9 @@ n_iter = 100
 
 #  Load test file
 x = rs.load("input.wav",sr=16000)[0]
-x = np.expand_dims(x,0)
+n_sample = len(x)
+duration = n_sample/16000
+x2 = np.expand_dims(x,0)
 
 
 #  Load models
@@ -18,15 +20,16 @@ m1 = TRANetEnhancer("mpANC_v99.onnx")
 m2 = NSNet2_infer()
 
 # estimatte RTF
-
-tic = time.time()
-for i in tqdm(range(n_iter)) : 
-    y = m1(x)
-toc = time.time()
-print("RTF TRANet : {}".format((toc-tic)/n_iter))
-
 tic = time.time()
 for i in tqdm(range(n_iter)) : 
     y = m2(x)
 toc = time.time()
-print("RTF NSNet2 : {}".format((toc-tic)/n_iter))
+print("RTF NSNet2 : {}".format((toc-tic)/n_iter/duration))
+
+
+tic = time.time()
+for i in tqdm(range(n_iter)) : 
+    y = m1(x2)
+toc = time.time()
+print("RTF TRANet : {}".format((toc-tic)/n_iter/duration))
+
